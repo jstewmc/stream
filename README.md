c8058e8bbb5bfae65cfbe821c3e04b6ecb7c337f
Jack Clayton <clayjs0@gmail.com>
2015-03-23 20:04:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Stream
+
+Stream a text file character-by-character.
+
+For example:
+
+```php
+use Jstewmc\Stream;
+
+// create a trivial text file
+file_put_contents('path\to\file.txt', 'foo');
+
+// create a new stream
+$stream = new File('path\to\file.txt');
+
+// while a current character exists
+while ($stream->getCurrentCharacter()) {
+	// echo the current character to the screen
+	echo "\"$stream->getCurrentCharacter()\""."\n";	
+	// advance to the next character
+	$stream->getNextCharacter();
+}
+```
+
+The example above would produce the following output:
+
+```
+"f"
+"o"
+"o"
+```
+
+Of course, this example is trivial. However, for very large files (on the order of several megabytes), storing the entire file's contents in memory as a string or as an array can be memory intensive. With the `Stream` class, you can loop through the file character-by-character.
+
+## Methodology
+
+Basically, this library divides very large text files into chunks. As you move character-to-character, it get and splits the next or previous chunk in the background as needed.
+
+## Methods
+
+You can set the file's name via the constructor or the `setName()` method:
+
+```php
+use Jstewmc\Stream;
+
+$a = new File('path\to\file.txt');
+
+$b = new File();
+$b->setName('path\to\file.txt');
+
+$a == $b;  // returns true
+```
+
+Keep in mind, however you set the file's name, the file must exist and be readable. Otherwise, an `InvalidArgumentException` will be thrown.
+
+You can get the file's current, next, and previous characters:
+
+```php
+use Jstewmc\Stream;
+
+file_put_contents('path\to\file.txt', 'foo');
+
+$stream = new File('path\to\file.txt');
+
+$stream->getCurrentCharacter();   // returns "f"
+$stream->getNextCharacter();      // returns "o"
+$stream->getPreviousCharacter();  // returns "f"
+```
+
+The `getNextCharacter()` and `getPreviousCharacter()` methods behave like PHP's native `next()` and `prev()` methods. When called, they'll increment or decrement the internal pointer and return the corresponding character.
+
+You can reset the stream's internal pointer:
+
+```php
+use Jstewmc\Stream;
+
+file_put_contents('path\to\file.txt', 'foo');
+
+$stream = new File('path\to\file.txt');
+
+$stream->getCurrentCharacter();  // returns "f"
+$stream->getNextCharacter();     // returns "o"
+
+$stream->reset();
+
+$stream->getCurrentCharacter();  // returns "f"
+```
+
+## About
+
+In February 2015, I wrote a library to [read and write RTF files](https://github.com/jstewmc/rtf). Unfortunately, I soon realized that some RTF files can be very large, too large to get and split as one string.
+
+## Contributing
+
+See [CONTRIBUTING.md](https://github.com/jstewmc/stream/blob/master/CONTRIBUTING.md) for details.
+
+## Author
+
+Jack Clayton - [clayjs0@gmail.com](mailto:clayjs0@gmail.com)
+
+## License
+
+This library is released under the [MIT license](https://github.com/jstewmc/stream/blob/master/LICENSE).
+
+## Version
+
+0.1.0 - See [CHANGELOG.md](https://github.com/jstewmc/stream/blob/master/CHANGELOG.md) for details.
---
+++ src/Stream.php
@@ -0,0 +1,337 @@
+<?php
+/**
+ * The Jstewmc\Stream\Stream class file
+ *
+ * @author     Jack Clayton <clayjs0@gmail.com>
+ * @copyright  2015 Jack Clayton
+ * @license    MIT
+ */
+
+namespace Jstewmc\Stream;
+
+/**
+ * The stream class
+ *
+ * The Stream class allows you treat the contents of files and strings as a
+ * continuous flow of characters. That is, you can iterate over the characters 
+ * in a very large file or string without holding the contents of the file as an 
+ * array in memory.
+ *
+ * @since  0.1.0
+ */
+abstract class Stream
+{
+	/* !Protected properties */
+	
+	/**
+	 * @var  string[]  an array of the current chunk's characters
+	 * @since  0.1.0
+	 */
+	protected $characters;
+	
+	/**
+	 * @var  int  the current chunk's index; defaults to 0
+	 * @since  0.1.0
+	 */
+	protected $chunkIndex = 0;
+	
+	/**
+	 * @var  int  the (maximum) chunk size; defaults to 8 kilobytes (8 * 1024 bytes)
+	 * @since  0.1.0
+	 */
+	protected $chunkSize = 8192;
+	
+	
+	/* !Get methods */
+	
+	/**
+	 * Returns the current chunk's characters
+	 *
+	 * @return  string[]|null
+	 * @since  0.1.0
+	 */
+	public function getCharacters()
+	{
+		return $this->characters;
+	}
+	
+	/**
+	 * Returns the current chunk's index
+	 *
+	 * @return  int|null
+	 * @since  0.1.0
+	 */
+	public function getChunkIndex()
+	{
+		return $this->chunkIndex;
+	}
+
+	/**
+	 * Returns the (maximum) chunk size
+	 *
+	 * @return  string|null
+	 * @since  0.1.0
+	 */
+	public function getChunkSize()
+	{
+		return $this->chunkSize;
+	}
+	
+	
+	/* !Set methods */
+	
+	/**
+	 * Sets the chunk's current characters
+	 *
+	 * @param  string[]  an array of characters
+	 * @return  self
+	 * @throws  InvalidArgumentException  if $characters is not an array
+	 * @throws  InvalidArgumentException  if $characters is not an array of strings
+	 * @since  0.1.0
+	 */
+	public function setCharacters($characters)
+	{
+		if ( ! is_array($characters) 
+			|| count($characters) !== count(array_filter($characters, 'is_string'))
+		) {
+			throw new \InvalidArgumentException(
+				__METHOD__."() expects parameter one, characters, to be an array of strings"
+			);	
+		}
+		
+		$this->characters = $characters;
+		
+		return $this;
+	}
+	
+	/**
+	 * Sets the chunk index
+	 *
+	 * @param  int  $chunkIndex  the chunk's index
+	 * @return  self
+	 * @throws  InvalidArgumentException  if $chunkIndex is not an integer
+	 * @since  0.1.0
+	 */
+	public function setChunkIndex($chunkIndex)
+	{
+		if ( ! is_numeric($chunkIndex) || ! is_int(+$chunkIndex) || $chunkIndex < 0) {
+			throw new \InvalidArgumentException(
+				__METHOD__."() expects parameter one, index, to be a position integer or zero"
+			);
+		}
+		
+		$this->chunkIndex = $chunkIndex;
+		
+		return $this;
+	}
+	
+	/**
+	 * Sets the (maximum) chunk size
+	 *
+	 * @param  int  $chunkSize  the (maximum) chunk size
+	 * @return  self
+	 * @throws  InvalidArgumentException  if $size is not a positive, non-zero integer
+	 * @since  0.1.0
+	 */
+	public function setChunkSize($chunkSize)
+	{
+		if ( ! is_numeric($chunkSize) || ! is_int(+$chunkSize) || $chunkSize <= 0) {
+			throw new \InvalidArgumentException(
+				__METHOD__."() expects parameter one, size, to be a positive, non-zero integer"
+			);	
+		}
+		
+		$this->chunkSize = $chunkSize;
+		
+		return $this;
+	}
+	
+	
+	/* !Public methods */
+	
+	/**
+	 * An alias for the getCurrentCharacter() method
+	 *
+	 * @return  string|null
+	 * @since   0.1.0
+	 */
+	public function current()
+	{
+		return $this->getCurrentCharacter();	
+	}
+	
+	/**
+	 * Returns the current character
+	 *
+	 * @return  string|false
+	 * @since   0.1.0
+	 */
+	public function getCurrentCharacter()
+	{
+		$this->beforeGet();
+			
+		return current($this->characters);
+	}
+	
+	/**
+	 * Returns the next character
+	 *
+	 * @return  string|false
+	 * @since   0.1.0
+	 */
+	public function getNextCharacter()
+	{
+		$this->beforeGet();
+	
+		if ( ! $this->hasNextCharacter()) {
+			$this->readNextChunk();
+			$next = reset($this->characters);
+		} else {
+			$next = next($this->characters);
+		}
+		
+		return $next;
+	}
+	
+	/**
+	 * Returns the previous character
+	 *
+	 * @return  string|false
+	 * @since   0.1.0
+	 */
+	public function getPreviousCharacter()
+	{
+		$this->beforeGet();
+		
+		if ( ! $this->hasPreviousCharacter()) {
+			$this->readPreviousChunk();
+			$previous = end($this->characters);
+		} else {
+			$previous = prev($this->characters);
+		}
+		
+		return $previous;
+	}
+	
+	/**
+	 * An alias for the getNextCharacter() method
+	 *
+	 * @return  string|null
+	 * @since   0.1.0
+	 */
+	public function next()
+	{
+		return $this->getNextCharacter();
+	}
+	
+	/**
+	 * An alias for the getPreviousCharacter() method
+	 *
+	 * @return  string|null
+	 * @since   0.1.0
+	 */
+	public function prev()
+	{
+		return $this->getPreviousCharacter();
+	}
+	
+	/**
+	 * Resets the streams internal pointer
+	 *
+	 * @return  void
+	 * @since  0.1.0
+	 */
+	public function reset()
+	{
+		$this->chunkIndex = 0;
+		$this->characters = null;
+		
+		return;
+	}
+	
+	
+	/* !Protected methods */
+	
+	/**
+	 * Called before getting a character
+	 *
+	 * On the first call to a getXCharacter() method, the $characters array will be 
+	 * null, and I'll initialize it to the first chunk.
+	 * 
+	 * @return  void
+	 */
+	protected function beforeGet()
+	{
+		if ($this->characters === null) {
+			$this->readCurrentChunk();
+		}
+		
+		return;
+	}
+	
+	/**
+	 * Returns true if a next character exists in the current chunk
+	 *
+	 * @return  bool
+	 * @since   0.1.0
+	 */
+	protected function hasNextCharacter()
+	{
+		return key($this->characters) !== null 
+			&& array_key_exists(key($this->characters) + 1, $this->characters);
+	}
+	
+	/**
+	 * Returns true if a previous character exists in the current chunk
+	 * 
+	 * @return  bool
+	 * @since   0.1.0
+	 */
+	protected function hasPreviousCharacter()
+	{
+		return key($this->characters) !== null 
+			&& array_key_exists(key($this->characters) - 1, $this->characters);
+	}
+	
+	/**
+	 * Reads a chunk
+	 *
+	 * @param  string  $chunk  the string chunk to read
+	 * @return  void
+	 * @since  0.1.0
+	 */
+	protected function readChunk($chunk)
+	{
+		if (is_string($chunk) && ! empty($chunk)) {
+			$this->characters = str_split($chunk);
+		} else {
+			$this->characters = [];
+		}
+		
+		return;
+	}
+	
+	/**
+	 * Reads the current chunk into memory
+	 *
+	 * @return  void
+	 * @since  0.1.0
+	 */
+	abstract protected function readCurrentChunk();
+	
+	/**
+	 * Reads the next chunk into memory
+	 *
+	 * @return  void
+	 * @since  0.1.0
+	 */
+	abstract protected function readNextChunk();
+	
+	/**
+	 * Reads the previous chunk into memory
+	 *
+	 * @return  void
+	 * @since  0.1.0
+	 */
+	abstract protected function readPreviousChunk();
+}
